9fd37fcdc16014a4e9f3e9f50ef46510b3545556
Misash <misash@outlook.com>
2022-07-24 19:28:15 -0500

    aticles/ put

--- app.js
+++ app.js
@@ -10,77 +10,99 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(express.static("public"));
 
 
 //connect mongoDB
-mongoose.connect("mongodb://localhost:27017/wikiDB",{useNewUrlParser:true})
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true })
 
-const articleSchema ={
-    title : String ,
-    content : String 
+const articleSchema = {
+    title: String,
+    content: String
 }
 
-const Article = mongoose.model("Article",articleSchema)
+const Article = mongoose.model("Article", articleSchema)
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
 
     res.send("wiki Rest-API")
 })
 
 
-app.get("/articles",(req,res)=>{
 
-    Article.find({},(err,foundArticles)=>{
-        if(err){
-            res.send(err)
-        }else{
-            res.send(foundArticles)
-        }
+app.route("/articles")
+    .get((req, res) => {
+
+        Article.find({}, (err, foundArticles) => {
+            if (err) {
+                res.send(err)
+            } else {
+                res.send(foundArticles)
+            }
+        })
+
+    })
+    .post((req, res) => {
+
+        console.log(req.body.title)
+        console.log(req.body.content)
+
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        })
+
+        newArticle.save((err) => {
+            if (!err) {
+                res.send("Succesfully added a  new article")
+            } else {
+                res.send(err)
+            }
+        })
+
+    })
+    .delete((req, res) => {
+        Article.deleteMany({}, (err) => {
+            if (!err) {
+                res.send("Succesfully deleted all articles")
+            } else {
+                res.send(err)
+            }
+        })
     })
 
-})
 
-
-app.post("/articles",(req,res)=>{
-   
-    console.log(req.body.title)
-    console.log(req.body.content)
-    
-    const newArticle = new Article({
-        title : req.body.title ,
-        content : req.body.content
+app.route("/articles/:articleTitle")
+    .get((req, res) => {
+        Article.findOne({ title: req.params.articleTitle }, (err, foundArticle) => {
+            if (foundArticle) {
+                res.send(foundArticle)
+            } else {
+                res.send("Article was not found")
+            }
+        })
     })
-
-    newArticle.save((err)=>{
-        if(!err){
-            res.send("Succesfully added a  new article")
-        }else{
-            res.send(err)
-        }
+    .put((req, res) => {
+        Article.replaceOne(
+            { title: req.params.articleTitle },
+            { title: req.body.title , content: req.body.content } ,
+             (err)=> {
+                if(!err){
+                    res.send("Successfully updated article")
+                }else{
+                    res.send(err)
+                }
+        })
     })
-
-})
-
-
-app.delete("/articles",(req,res)=>{
-    Article.deleteMany({},(err)=>{
-        if(!err){
-            res.send("Succesfully deleted all articles")
-        }else{
-            res.send(err)
-        }
-    })
-})
 
 
 
 //TODO
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
 });
 
 
